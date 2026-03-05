70732ef9758814ca1252749eb54b03b236bff2f7
Ashishkumar667 <ashishkumarinfo6@gmail.com>
2026-03-05 12:27:27 +0530

    added console to check console l0g

--- controllers/auth/auth.js
+++ controllers/auth/auth.js
@@ -56,6 +56,7 @@ export const LoginUser = async ( req, res) =>{
     const { email, password } = req.body;
     try{
         const user = await User.findOne({ email});
+        console.log("User found for login:", user);
         if(!user){
             return res.status(404).json({ error: "User not found" });
         }
