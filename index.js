379c5a3d9acbdff695243ce81f456bddf7b429cb
Adityakumar0723 <adityamishraself11@gmail.com>
2026-06-15 12:53:56 +0530

    Fix welcome message in the root route

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use("/api/stripe/webhook", express.raw({ type: 'application/json' }), stripe
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the AutoBrand API.This one ondemand agent ");
+    res.send("Welcome to the AutoBrand API.This one ondemand agent ondemand");
 });
 
 app.use('/api/v1', authenticationRoutes);
