2b56924816c0df568fd2b00df5a0d9c3b34b4db8
Adityakumar0723 <adityamishraself11@gmail.com>
2026-07-24 21:52:32 +0530

    Update welcome message in index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use("/api/stripe/webhook", express.raw({ type: 'application/json' }), stripe
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the AutoBrand API.This one ondemand agent ondemand");
+    res.send("Welcome to the AutoBrand API.This one ondemand agent ondemand api six ");
 });
 
 app.use('/api/v1', authenticationRoutes);
