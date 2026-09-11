b98ee45f5a3687ec29927e682e774c5ee7c5c0b1
Adityakumar0723 <adityamishraself11@gmail.com>
2026-09-11 16:34:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use("/api/stripe/webhook", express.raw({ type: 'application/json' }), stripe
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the AutoBrand API welcome back. ");
+    res.send("Welcome to the AutoBrand API welcome back ondemand is the father of ai . ");
 });
 
 app.use('/api/v1', authenticationRoutes);
